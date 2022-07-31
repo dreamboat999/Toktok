fae92605321bdb1cdf324dfec83cd36c387162d0
Kyle Yutatka <kyleyutaka@gmail.com>
2022-07-31 14:21:45 +0200

    add logo and utils

---
+++ components/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import {GoogleLogin, GoogleLogout} from 'react-google-login';
+import {AiOutlineLogout} from 'react-icons/ai';
+import {BiSearch} from 'react-icons/bi';
+import {IoMdAdd} from 'react-icons/io';
+
+import Logo from '../utils/grafftok-logo.png';
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link href="/">
+        <div>
+          <Image 
+          className="cursor-pointer"
+          src={Logo}
+          alt="GraffTok"
+          layout="responsive"
+          />
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="text-3xl font-bold underline">
       GraffTok
     </div>
   )
