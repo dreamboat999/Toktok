34fb31e7cfa967b1db56eb145239efe657af79f8
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-02 20:35:40 +0200

    style topic and active topic category

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -5,17 +5,24 @@ import {useRouter} from 'next/router';
 import {topics} from '../utils/constants';
 
 const Discover = () => {
+  const router = useRouter();
+  const {topic} = router.query;
+
+  const activeTopicStyle = "xl:border-2 hover:bg-primary xl:border-[#F51997] px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-[#F51997]"
+
+  const topicStyle = "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black"
+
   return (
     <div className="xl:border-b-2 xl:border-gray-200 pb-6">
       <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">Popular Topics</p>
       <div className="flex gap-3 flex-wrap">
         {topics.map((item) => (
           <Link href={`/?topic=${item.name}`} key={item.name}>
-            <div>
-              <span>
+            <div className={topic === item.name ? activeTopicStyle :topicStyle}>
+              <span className="font-bold text-2xl xl:text-base">
                 {item.icon}
               </span>
-              <span>
+              <span className="font-medium text-base hidden xl:block capitalize">
                 {item.name}
               </span>
             </div>
