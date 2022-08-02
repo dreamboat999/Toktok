29f280a295aaf8e168d266668c31bf811a912442
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-02 18:03:35 +0200

    display popular topics

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Link from 'next/link';
+import {useRouter} from 'next/router';
+
+import {topics} from '../utils/constants';
 
 const Discover = () => {
   return (
-    <div>Discover</div>
+    <div className="xl:border-b-2 xl:border-gray-200 pb-6">
+      <p className="text-gray-500 font-semibold m-3 mt-4 hidden xl:block">Popular Topics</p>
+      <div className="flex gap-3 flex-wrap">
+        {topics.map((item) => (
+          <Link href={`/?topic=${item.name}`} key={item.name}>
+            <div>
+              <span>
+                {item.icon}
+              </span>
+              <span>
+                {item.name}
+              </span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
   )
 }
 
