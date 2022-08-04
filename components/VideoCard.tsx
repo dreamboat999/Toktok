5fd40d38eb2124cbe98e75ca85d866947833d52a
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-04 20:23:15 +0200

    display profile of creator in video card, config next images

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
-import { Video } from '../types';
+import React,{ useState,useEffect,useRef } from 'react'
 import { NextPage } from 'next';
+import { Video } from '../types';
+import Image from 'next/image';
+import Link from 'next/link';
+import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import { GoVerified } from 'react-icons/go';
 
 interface IProps {
   post: Video;
@@ -8,7 +13,26 @@ interface IProps {
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
   return (
-    <div>VideoCard</div>
+    <div className="flex flex-col border-b-2 border-gray-200 pb-6">
+      <div>
+        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+          <div className="md:w-16 md:h-16 w-10 h-10">
+            <Link href="/">
+              <>
+                <Image 
+                  width={62}
+                  height={62}
+                  className="rounded-full"
+                  src={post.postedBy.image}
+                  alt="profile photo"
+                  layout="responsive"
+                />
+              </>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
