b11599f8e60686bfcb3a6eced50bd62c3f0c780e
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-06 14:37:09 +0200

    boilerplate to login by reactgoogleouath

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import {GoogleLogin, GoogleLogout} from 'react-google-login';
-import {AiOutlineLogout} from 'react-icons/ai';
-import {BiSearch} from 'react-icons/bi';
-import {IoMdAdd} from 'react-icons/io';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
 
 import Logo from '../utils/grafftok-logo.png';
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -22,6 +23,19 @@ const Navbar = () => {
           />
         </div>
       </Link>
+      <div>
+        SEARCH
+      </div>
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ) : (
+          <GoogleLogin
+           onSuccess={(response) => console.log(response)}
+           onError={() => console.log('Error')}
+           />
+        )}
+      </div>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import {useState, useEffect} from 'react';
 import { Navbar, Sidebar } from '../components';
-
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
@@ -15,7 +15,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if(isSSR) return null;
 
   return (
-  <div>
+  <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
     <Navbar />
     <div className="flex gap-6 md:gap-20">
       <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
@@ -25,7 +25,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <Component {...pageProps} />
       </div>
     </div>
-  </div>
+  </GoogleOAuthProvider>
   );
 }
 
