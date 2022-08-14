ef97459ec01f4fd2c9978995963054de05eb359a
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-14 21:36:08 +0200

    create form for adding caption and categories for movie

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -8,6 +8,8 @@ import { SanityAssetDocument } from '@sanity/client';
 import useAuthStore from '../store/authStore';
 import {client} from '../utils/client';
 
+import { topics } from '../utils/constants';
+
 const Upload = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [videoAsset, setVideoAsset] = useState<SanityAssetDocument | undefined>();
@@ -33,8 +35,8 @@ const Upload = () => {
   }
 
   return (
-    <div className="flex w-full h-full">
-      <div className="bg-white rounded-lg">
+    <div className="flex w-full h-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center">
+      <div className="bg-white rounded-lg xl:h-[80vh] w-[60%] flex gap-6 flex-wrap justify-between items-center p-14 pt-6">
         <div>
           <div>
             <p className="text-2xl font-bold">Upload Video</p>
@@ -93,7 +95,48 @@ const Upload = () => {
               </p>
             )}
           </div>
+         
         </div>
+        <div className="flex flex-col gap-3 pb-10">
+              <label className="text-base font-medium">Caption</label>
+              <input
+              type="text"
+              value="" 
+              onChange={() => {}}
+              className="rounded outline-none text-base border-2 border-gray-200 p-2"
+              />
+              <label className="text-base font-medium">Choose a Category</label>
+              <select
+              onChange={() => {}}
+              className="outline-none border-2 border-gray-200 text-base capitalize lg:p-4 p-2 rounded cursor-pointer"
+              >
+                {topics.map((topic) => (
+                  <option 
+                  key={topic.name}
+                  className="outline-none capitalize bg-white text-gray-700 text-base p-2 hover:bg-slate-300"
+                  value={topic.name}
+                  >
+                    {topic.name}
+                  </option>
+                ))}
+              </select>
+              <div className="flex gap-6 mt-10">
+                  <button
+                  onClick={() => {}}
+                  type="button"
+                  className="border-gray-300 border-2 text-base font-medium p-2 rounded w-28 lg:w-44 outline-none"
+                  >
+                    Discard
+                  </button>
+                  <button
+                  onClick={() => {}}
+                  type="button"
+                  className="bg-[#F51997] text-white text-base font-medium p-2 rounded w-28 lg:w-44 outline-none"
+                  >
+                    Post
+                  </button>
+              </div>
+          </div>
       </div>
     </div>
   )
