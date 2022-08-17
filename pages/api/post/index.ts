9aa6cb02eea65bc9acc4ea36218e575442f370b7
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-17 19:49:37 +0200

    handlePost fn

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -14,5 +14,10 @@ export default async function handler( req: NextApiRequest,
     const data = await client.fetch(query);
 
     res.status(200).json(data);
+  } else if (req.method === 'POST') {
+    const document = req.body;
+
+    client.create(document)
+    .then(() => res.status(201).json('VideoCreated'))
   }
 }
