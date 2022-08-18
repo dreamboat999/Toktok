3da733fd2f38fd5f7880401672381f31c7b0f545
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-18 23:56:40 +0200

    center whole app css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,6 +16,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
   <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
+    <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
     <Navbar />
     <div className="flex gap-6 md:gap-20">
       <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
@@ -25,6 +26,8 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <Component {...pageProps} />
       </div>
     </div>
+    </div>
+   
   </GoogleOAuthProvider>
   );
 }
