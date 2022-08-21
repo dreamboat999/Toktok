2379fec3c2d1b5ad5448232e3c3ce99c7bac6c82
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-21 16:03:29 +0200

    video tsx display

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,17 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
+import { postDetailQuery } from './../../../utils/queries';
 import { client } from '../../../utils/client';
-import { postDetailQuery } from '../../../utils/queries';
 
-export default async function handler( req: NextApiRequest,
-  res: NextApiResponse) {
-  if(req.method === 'GET') {
-    const {id} = req.query;
 
+export default async function handler( req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    const { id } = req.query;
     const query = postDetailQuery(id);
 
     const data = await client.fetch(query);
 
-    res.status(200).json([data[0]])
-  }
-}
+    res.status(200).json(data[0]);
+  }};
--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -16,9 +16,33 @@ interface IProps {
 
 const Detail = ({postDetails}: IProps) => {
   const [post,setPost] = useState(postDetails);
+  const videoRef = useRef(null);
+
+  if(!post) return null;
 
   return (
-    <div>detail page</div>
+    <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
+      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black">
+        <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
+          <p>
+            <MdOutlineCancel className="text-white text-[35px]"/>
+          </p>
+        </div>
+        <div className="relative">
+          <div className="lg:h-[100vh] h-[60vh]">
+            <video
+             src={post.video.asset.url}
+             ref={videoRef}
+             loop
+             onClick={() => {}}
+             className="h-full cursor-pointer"
+             >
+            </video>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
