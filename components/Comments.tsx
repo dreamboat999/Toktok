2485ad21e13d79c322e09392c04d6ce74b7e740b
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-22 18:49:07 +0200

    boilerplate for comments section

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { GoVerified } from 'react-icons/go'
+
+import useAuthStore from '../store/authStore';
+import NoResults from './NoResults';
 
 const Comments = () => {
+  const comments = [];
   return (
-    <div>Comments</div>
+    <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
+      <div className="overflow-scroll lg:h-[475px]">
+        {comments.length ? (
+          <div>videos</div>
+        ) : (
+          <NoResults text="No comments yet!"/>
+        )}
+      </div>
+    </div>
   )
 }
 
--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -23,7 +23,7 @@ const LikeButton = ({likes, handleLike, handleDislike}: IProps) => {
   },[filterLikes, likes])
 
   return (
-    <div className="gap-6">
+    <div className="flex gap-6">
       <div className="mt-4 flex flex-col justify-center items-center cursor-pointer">
         {alreadyLiked ? (
           <div className="bg-primary rounded-full p-2 md:p-4 text-[#F51997]" onClick={handleDislike}>
