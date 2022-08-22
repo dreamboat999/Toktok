9390bdb328cdd0423ac5871e699ccbe2e8a93c24
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-22 20:13:01 +0200

    no results component

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {MdOutlineVideocamOff} from 'react-icons/md'
+import {BiCommentX} from 'react-icons/bi'
 
 interface IProps {
   text: string;
@@ -6,7 +8,16 @@ interface IProps {
 
 const NoResults = ({ text }: IProps) => {
   return (
-    <div>NoResults</div>
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <p className="text-8xl">
+        {text === 'No comments yet!'
+         ? <BiCommentX /> 
+         :<MdOutlineVideocamOff />}
+      </p>
+      <p className="text-2xl text-center">
+        {text}
+      </p>
+    </div>
   )
 }
 
