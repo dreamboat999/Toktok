19e480bdd559fb95804490e60554f8e488c1bf7f
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-24 14:56:08 +0200

    create posting comment and display it

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,6 +5,7 @@ import { GoVerified } from 'react-icons/go'
 
 import useAuthStore from '../store/authStore';
 import NoResults from './NoResults';
+import { IUser } from '../types';
 
 interface IProps {
   isPostingComment: Boolean;
@@ -22,15 +23,49 @@ interface IComment {
 }
 
 const Comments = ({comment, setComment, addComment, comments, isPostingComment}: IProps) => {
-  const {userProfile} = useAuthStore();
+  const {userProfile, allUsers} = useAuthStore();
 
   return (
     <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
       <div className="overflow-scroll h-[300px]">
-        {comments?.length ? (
-          <div>videos</div>
+      {comments?.length > 0 ? (
+          comments?.map((item: IComment, idx: number) => (
+            <>
+              {allUsers?.map(
+                (user: IUser) =>
+                  user._id === (item.postedBy._ref || item.postedBy._id) && (
+                    <div className=' p-2 items-center' key={idx}>
+                      <Link href={`/profile/${user._id}`}>
+                        <div className='flex items-start gap-3'>
+                          <div className='w-12 h-12'>
+                            <Image
+                              width={48}
+                              height={48}
+                              className='rounded-full cursor-pointer'
+                              src={user.image}
+                              alt='user-profile'
+                              layout='responsive'
+                            />
+                          </div>
+
+                          <p className='flex cursor-pointer gap-1 items-center text-[18px] font-bold leading-6 text-primary'>
+                            {user.userName}{' '}
+                            <GoVerified className='text-blue-400' />
+                          </p>
+                        </div>
+                      </Link>
+                      <div>
+                        <p className='-mt-5 ml-16 text-[16px] mr-8'>
+                          {item.comment}
+                        </p>
+                      </div>
+                    </div>
+                  )
+              )}
+            </>
+          ))
         ) : (
-          <NoResults text="No comments yet!"/>
+          <NoResults text='No comments yet!' />
         )}
       </div>
       {userProfile && (
