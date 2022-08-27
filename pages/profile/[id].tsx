ce247429b4687408651c87957a5429bd9fc81656
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-27 14:34:00 +0200

    jsx of profile page name of user and videos/liked section

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -16,8 +16,42 @@ interface IProps {
 }
 
 const Profile = ({data}: IProps) => {
+  const [showUserVideos, setShowUserVideos] = useState(true);
+  const { user, userVideos, userLikedVideos } = data;
+
+  const videos = showUserVideos ? 'border-b-2 border-black' : 'text-gray-400'
+  const liked = !showUserVideos ? 'border-b-2 border-black' : 'text-gray-400'
+
   return(
-    <div>profile</div>
+    <div className="w-full">
+      <div className="flex gap-6 md:gap-10 mb-4 bg-white w-full">
+        <div className="w-16 h-16 md:w-32 md:h-32">
+          <Image 
+          src={user.image}
+          width={120}
+          height={120}
+          className="rounded-full"
+          alt="user profile"
+          layout="responsive"
+          />
+        </div>
+        <div className="flex flex-col justify-center">
+          <p className="md:text-2xl tracking-wider flex gap-1 items-center justify-center text-base font-bold text-primary lowercase">
+          {user.userName.replaceAll(' ', '')}
+          <GoVerified className="text-blue-400"/>
+          </p>
+          <p className="capitalize md:text-xl text-gray-400 text-xs">
+          {user.userName}
+          </p>
+        </div>  
+      </div>
+      <div>
+        <div className="flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full">
+          <p className={`text-xl font-semibold cursor-pointer mt-2 ${videos}`} onClick={() => setShowUserVideos(true)}>Videos</p>
+          <p className={`text-xl font-semibold cursor-pointer mt-2 ${liked}`} onClick={() => setShowUserVideos(false)}>Liked</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
