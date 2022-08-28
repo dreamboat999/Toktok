9354f21cf0eab76b990abba6a50bd08903eedb80
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-28 11:46:23 +0200

    boilerplate searchbar display

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,6 +15,10 @@ import useAuthStore from '../store/authStore';
 const Navbar = () => {
   const { userProfile, addUser, removeUser } = useAuthStore();
 
+  const handleSearch = () => {
+
+  }
+
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -27,8 +31,25 @@ const Navbar = () => {
           />
         </div>
       </Link>
-      <div>
-        SEARCH
+      <div className="relative hidden md:block">
+        <form
+        onSubmit={handleSearch}
+        className="absolute md:static top-10 -left-20 bg-white"
+        >
+          <input 
+          type="text" 
+          value=""
+          onChange={() => {}}
+          placeholder="Search accounts and videos"
+          className="bg-primary p-3 md:text-base font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full md:top-0"
+          />
+          <button
+          onClick={handleSearch}
+          className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
+          >
+            <BiSearch />
+          </button>
+        </form>
       </div>
       <div>
         {userProfile ? (
