e5a1f13ed6883a0e0c1be0e25342b11d797a60da
Kyle Yutatka <kyleyutaka@gmail.com>
2022-08-28 20:47:28 +0200

    seting display topics on main page

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -13,10 +13,12 @@ import useAuthStore from '../../store/authStore';
 const Search = ({videos}: {videos: Video[]}) => {
   const [isAccounts, setIsAccounts] = useState(false);
   const router = useRouter();
-  const {searchTerm} = router.query;
+  const {searchTerm}: any = router.query;
+  const {allUsers} = useAuthStore();
 
   const accounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400'
   const isVideos = !isAccounts ? 'border-b-2 border-black' : 'text-gray-400'
+  const searchedAccounts = allUsers.filter((user: IUser) => user.userName.toLowerCase().includes(searchTerm.toLowerCase()))
 
   return (
     <div className="w-full">
@@ -25,8 +27,33 @@ const Search = ({videos}: {videos: Video[]}) => {
           <p className={`text-xl font-semibold cursor-pointer mt-2 ${isVideos}`} onClick={() => setIsAccounts(false)}>Videos</p>
         </div>
         {isAccounts ? (
-          <div>
-            ACCOUNTNS
+          <div className="md:mt-16">
+            {searchedAccounts.length > 0 ? (
+              searchedAccounts.map((user: IUser, index: number) => (
+                 <Link href={`/profile/${user._id}`} key={index}>
+                        <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded border-b-2 border-gray-200'>
+                          <div>
+                            <Image
+                              width={50}
+                              height={50}
+                              className='rounded-full cursor-pointer'
+                              src={user.image}
+                              alt='user-profile'
+                            />
+                          </div>
+                          <div className='flex flex-col justify-center'>
+                          <p className='flex cursor-pointer gap-1 items-center text-[18px] font-bold leading-6 text-primary'>
+                            {user.userName}{' '}
+                            <GoVerified className='text-blue-400' />
+                          </p>
+                          <p className="capitalize text-gray-400 text-xs">
+                            {user.userName}
+                          </p>
+                          </div>
+                        </div>
+                      </Link>
+              ))
+            ) : <NoResults text={`No video results for ${searchTerm}`}/>}
           </div>
         ) : <div className="md:mt-16 flex flex-wrap gap-6 md:justfiy-start">
           {videos.length ? (
